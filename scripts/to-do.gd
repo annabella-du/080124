f507753297f1dd7ef4906f3355141f3ed3a837fe
annabella-du <annabelladu1234@gmail.com>
2024-08-07 22:08:13 -0400

    080724 3

--- scripts/to-do.gd
+++ scripts/to-do.gd
@@ -3,7 +3,14 @@ extends Node
 """
 To-do:
 	level design
+		camera limits
 		tutorial level
+			movement
+			ladder
+			enemies/stun
+			checkpoints
+			coins/levers
+			chests/keys/gates
 		1-2 levels
 		global switch scene function
 	basic ui
@@ -11,6 +18,9 @@ To-do:
 		pause menu
 		level select menu
 		credits
+	fix player appear animation height
+	make more detailed levels (esp tutorial lvl)
+	pickup hearts
 Future ideas:
 	teleport
 	cranks (linked system)
